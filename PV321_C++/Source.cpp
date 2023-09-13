854d3b7ebad7caf083cb2f4a6b81f43e83e67da3
Сергій Гололобов <gololobov.serhiy@gmail.com>
2023-09-13 21:01:37 +0300

    13.09.2023

--- PV321_C++/Source.cpp
+++ PV321_C++/Source.cpp
@@ -13,6 +13,35 @@ enum SUIT
 	SPADE = 1, CLUB, DIAMOND, HEARH
 };
 
+
+// type name(param)
+// {
+// 	  body;
+// }
+
+
+void starLine()
+{
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+}
+
+int sum(int a, int b)
+{
+	int c = a + b;
+	return c;
+}
+
+float avg3(int a, int b, int c)
+{
+	float s = (a + b + c) / 3.;
+	return s;
+}
+
+
 int main()
 {
 	SetConsoleCP(1251);
@@ -22,12 +51,50 @@ int main()
 
 	srand(time(0));
 
-	const int size = 10;
+
+	starLine();
+	//
+	starLine();
+
+	int c = sum(3, 5);
+	cout << sum(3, 5) << endl;
+
+
+	cout << avg3(3, 4, 6) << endl;
+
+
+	/*const int size = 10;
 	int arr[size];
 	for (size_t i = 0; i < size; i++)
 	{
 		arr[i] = rand() % 10;
 	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;*/
+
+
+	////// sort  ////////
+
+	/*for (size_t i = 0; i < size - 1; i++)
+	{
+		for (size_t j = 0; j < size - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;*/
 
 	/*for (size_t i = 0; i < size; i++)
 	{
@@ -38,7 +105,7 @@ int main()
 	/*int count0 = 0;
 	for (size_t i = 0; i < size; i++)
 	{
-		if (arr[i] == 0) 
+		if (arr[i] == 0)
 		{
 			count0++;
 		}
@@ -77,7 +144,7 @@ int main()
 			imin = i;
 		}
 	}
-	
+
 	int start = (imax > imin) ? imin : imax;
 	int end = (imax > imin) ? imax : imin;
 
@@ -95,11 +162,11 @@ int main()
 	b = t;*/
 	//swap(a, b);
 
-	int B[size];
+	/*int B[size];
 	int c = 0;
 	for (size_t i = 0; i < size; i++)
 	{
-		if (arr[i] % 2 == 0) 
+		if (arr[i] % 2 == 0)
 		{
 			B[c++] = arr[i];
 		}
@@ -110,14 +177,109 @@ int main()
 		cout << arr[i] << " ";
 	}
 	cout << endl;
-	
+
 	for (int i = 0; i < c; i++)
 	{
 		cout << B[i] << " ";
 	}
+	cout << endl;*/
+
+
+
+	// type name[row][col];
+
+	//const int row = 4;
+	//const int col = 3;
+	//int arr[row][col]; // = { {1,2,3}, {4,5}, {6,7} };
+
+	//for (size_t i = 0; i < row; i++)
+	//{
+	//	for (size_t j = 0; j < col; j++)
+	//	{
+	//		arr[i][j] = rand() % 10;
+	//	}
+	//}
+
+
+	//for (size_t i = 0; i < row; i++)
+	//{
+	//	for (size_t j = 0; j < col; j++)
+	//	{
+	//		cout << arr[i][j] << " ";
+	//	}
+	//	cout << endl;
+	//}
+
+
+	/*int maxSum = INT_MIN, iRow = 0;
+	for (size_t i = 0; i < row; i++)
+	{
+		int sum = 0;
+		for (size_t j = 0; j < col; j++)
+		{
+			sum += arr[i][j];
+		}
+		if (sum > maxSum)
+		{
+			maxSum = sum;
+			iRow = i;
+		}
+	}
+	cout << "Row = " << iRow << endl;*/
+
+
+
+	/*int minSum = INT_MAX, iCol = 0;
+	for (size_t j = 0; j < col; j++)
+	{
+		int sum = 0;
+		for (size_t i = 0; i < row; i++)
+		{
+			sum += arr[i][j];
+		}
+		if (sum < minSum)
+		{
+			minSum = sum;
+			iCol = j;
+		}
+	}
+	cout << "Col = " << iCol << endl;*/
+
+
+	/*int sum[row];
+	for (size_t i = 0; i < row; i++)
+	{
+		int s = 0;
+		for (size_t j = 0; j < col; j++)
+		{
+			s += arr[i][j];
+		}
+		sum[i] = s;
+	}
+
+	for (size_t i = 0; i < row - 1; i++)
+	{
+		for (size_t j = 0; j < row - 1 - i; j++)
+		{
+			if (sum[j] < sum[j + 1])
+			{
+				swap(sum[j], sum[j + 1]);
+				for (size_t k = 0; k < col; k++)
+				{
+					swap(arr[j][k], arr[j+1][k]);
+				}
+			}
+		}
+	}
 	cout << endl;
 
-
-
+	for (size_t i = 0; i < row; i++)
+	{
+		for (size_t j = 0; j < col; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}*/
 
 }
